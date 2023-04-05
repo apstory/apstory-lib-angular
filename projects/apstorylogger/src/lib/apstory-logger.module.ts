4dc2f652b32ee3bf6962320c0412a7b381bb9e53
jacoapstory <44868579+jacoapstory@users.noreply.github.com>
2023-04-05 08:18:01 +0200

    Add ApstoryLoggerModule ignoreClientErrors field in forRoot method

--- projects/apstorylogger/src/lib/apstory-logger.module.ts
+++ projects/apstorylogger/src/lib/apstory-logger.module.ts
@@ -8,11 +8,14 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
   exports: []
 })
 export class ApstoryLoggerModule {
-  static forRoot(instrumentationKey: string, loggingLevelConsole: number): ModuleWithProviders<ApstoryLoggerModule> {
+  static forRoot(instrumentationKey: string, loggingLevelConsole: number, ignoreClientErrors: boolean = false): ModuleWithProviders<ApstoryLoggerModule> {
     return {
       ngModule: ApstoryLoggerModule,
-      providers: [ApstoryLoggerService, { provide: 'instrumentationKey', useValue: instrumentationKey },
-        { provide: 'loggingLevelConsole', useValue: loggingLevelConsole }]
+      providers: [ApstoryLoggerService, 
+        { provide: 'instrumentationKey', useValue: instrumentationKey },
+        { provide: 'loggingLevelConsole', useValue: loggingLevelConsole },
+        { provide: 'ignoreClientErrors', useValue: ignoreClientErrors },
+      ]
     };
   }
 }
